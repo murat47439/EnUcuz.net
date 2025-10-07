1d14ea92485908cb5d160bbe96b90cca10d6985b
Murat Turan <murat47439@gmail.com>
2025-10-07 21:31:54 +0300

    fix 4

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
-'use client';
 import {Suspense } from "react"
 import HomePageContent from "@/features/components/content/homePageContent"
+
+export const dynamic = 'force-dynamic'
 
 export default function HomePage() {
   return (
