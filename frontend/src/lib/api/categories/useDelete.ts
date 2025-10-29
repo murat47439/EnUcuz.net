a12cb72df04f4c57fc578828c95e5d29f5d549aa
Murat Turan <murat47439@gmail.com>
2025-10-29 18:01:58 +0300

    {api}

--- frontend/src/lib/api/categories/useDelete.ts
+++ frontend/src/lib/api/categories/useDelete.ts
@@ -1,5 +1,5 @@
 import { AxiosError } from "axios";
-import { api} from "../apiClient";
+import api from "../apiClient";
 import { Message, IdParam } from "@/lib/types/types";
 
 export async function DeleteCategory(data: IdParam){
